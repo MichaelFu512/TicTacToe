be7c3a1ad7f1f20fe8682bbefb207730a9fdee27
Michael Fu <michaelfu512@gmail.com>
2020-07-14 17:26:30 -0700

    Added method to place symbols and a loop that asks user to add symbol.

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,36 +1,87 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 	private static final int firstSpot = 1;
 	private static final int secondSpot = 2;
 	private static final int thirdSpot = 3;
 
 	public static void main(String[] args) {
-		boolean finishedGame = false;
-		
+		boolean gameDone = false;
 		char[][] gameBoard = {{' ', '|', ' ', '|', ' '}, 
 				{'-', '+', '-', '+', '-'}, 
 				{' ', '|', ' ', '|', ' '}, 
-				{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'} };
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '}};
 
 		printGameBoard(gameBoard);
-				
-		choiceOfPositioning(gameBoard, 'X');
-		System.out.println(gameBoard[0][0]);
+		
+		while(gameDone == false) {
+			choiceOfPositioning(gameBoard, 'X');
+			choiceOfPositioning(gameBoard, 'O');
+		}
 	
 		printGameBoard(gameBoard);
 
 	}
 	
 	public static void choiceOfPositioning(char[][] gameBoard, char symbol) {
+		boolean loop = true;
+		while(loop == true) {
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.print("Enter the row you want to input in (1-3): ");
+			int rowPosition = scan.nextInt();
+			
+			while(rowPosition > thirdSpot || rowPosition < firstSpot) {
+				System.out.println("Invalid row! Please enter a valid row. ");
+				System.out.print("Enter the row you want to input in (1-3): ");
+				rowPosition = scan.nextInt();
+			}
+			
+			System.out.print("Enter the column you want to input in (1-3): ");
+			int colPosition = scan.nextInt();
+			
+			while(colPosition > thirdSpot || colPosition < firstSpot) {
+				System.out.println("Invalid column! Please enter a valid column. ");
+				System.out.print("Enter the column you want to input in (1-3): ");
+				colPosition = scan.nextInt();
+			}
+			
+			rowPosition = positionCorrection(rowPosition);
+			colPosition = positionCorrection(colPosition);
+			
+			if(gameBoard[rowPosition][colPosition] == ' ') {
+				gameBoard[rowPosition][colPosition] = symbol;
+				printGameBoard(gameBoard);
+				loop = false;
+			}
+			
+			else {
+				System.out.println("That spot already has a symbol, try another spot");
+				printGameBoard(gameBoard);
+			}
+		}
+	}
+	
+	public static int positionCorrection(int position) {
+		if(position == firstSpot) {
+			position = position - 1;
+		}
+		else if(position == thirdSpot) {
+			position = position + 1;
+		}
+		return position;
 	}
 	
 	public static void printGameBoard(char[][] gameBoard) {
+		System.out.println();
 		for(char[] row : gameBoard) {
 			for(char c : row) {
 				System.out.print(c);
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
 
 }
